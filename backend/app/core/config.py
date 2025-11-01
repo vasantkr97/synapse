e96f34761c8115eadd43efa49bff6bbcc3df44e9
vasanth <vasantkr97@gmail.com>
2025-11-01 18:21:22 +0530

    security

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -5,6 +5,8 @@ class Settings(BaseSettings):
     VERSION: str = "v1.0.0"
     DATABASE_URL: str
     SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
 
     class Config:
         env_file = ".env"
