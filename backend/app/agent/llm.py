25fd638dddbd3b2befde638b81e179551f1059d9
vasanth <vasantkr97@gmail.com>
2025-11-13 16:25:19 +0530

    apis

--- backend/app/agent/llm.py
+++ backend/app/agent/llm.py
@@ -7,7 +7,7 @@ load_dotenv()
 api_key = os.getenv("GEMINI_API_KEY")
 
 if not api_key:
-    raise ValueError("OPENAI_API_KEY not found in environment variables")
+    raise ValueError("GEMINI_API_KEY not found in environment variables")
 
 gemini_llm = ChatGoogleGenerativeAI(model="gpt-4o", temperature=0)
 
